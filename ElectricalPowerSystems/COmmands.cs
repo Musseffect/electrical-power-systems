f82c8777a938a58376903f5ecf5d3819fbf2b251
Musseffect <lazarevich356@gmail.com>
2019-12-29 21:46:38 +0700

    GUI stuff

---
+++ ElectricalPowerSystems/COmmands.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ElectricalPowerSystems
+{
+    public static class Commands
+    {
+        public static readonly RoutedUICommand Exit = new RoutedUICommand
+            (
+                "Exit",
+                "Exit",
+                typeof(Commands)
+            );
+
+
+    }
+}
